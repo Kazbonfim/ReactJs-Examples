f396ba5a0300914c3e2bbf40eb4bd33d8f4f1f83
Kazbonfim <zcry.games@gmail.com>
2025-01-07 04:26:14 -0300

    Aula finalizada!

--- Aula02/src/components/JSXExamples.jsx
+++ Aula02/src/components/JSXExamples.jsx
@@ -3,28 +3,34 @@ import React from 'react'
 const JSXExamples = () => {
 
     const pokemon1 = {
+        id: 1,
         name: "Pikachu",
         type: "Elétrico",
         level: "20"
     }
 
     const pokemon2 = {
+        id: 2,
         name: "Bulbasaur",
         type: "Grama/Veneno",
         level: "15"
     }
 
     const pokemon3 = {
+        id: 3,
         name: "Charmander",
         type: "Fogo",
         level: "18"
     }
+
 
     function falarNome(nome) {
         return nome
     }
 
     const userIsOnline = true;
+
+    const lista = [pokemon1, pokemon2, pokemon3];
 
     return (
         <div className='container text-dark text-center'>
@@ -71,7 +77,8 @@ const JSXExamples = () => {
 
             {/* Usando as funções simples do HTML dentro de elementos em JSX */}
             <div>
-                <h1>Olá, {falarNome(pokemon1.name)}!</h1>
+                <h1 className='text-primary'>Usando funções dentro do JSX</h1>
+                <h2>Olá, {falarNome(pokemon1.name)}!</h2>
                 <br />
                 <div className="container d-flex justify-content-evenly">
                     <button onClick={() => { alert(`Você escolheu o ${pokemon1.name}`) }} className="btn btn-success">
@@ -90,6 +97,7 @@ const JSXExamples = () => {
 
             {/* Graças a Deus, existem condições aqui como existe no  Handlebars!!! */}
             <div className='container mb-5'>
+                <h2 className='text-primary'>Usando condicionais dentro do JSX</h2>
                 <h1>Usuário está Online?</h1>
                 <br />
                 <div className='container d-flex justify-content-evenly'>
@@ -112,6 +120,17 @@ const JSXExamples = () => {
                 </div>
             </div>
 
+            <hr />
+
+            {/* Trabalhando com listas, por exemplo... */}
+            <div className="container text-center mb-5">
+                <h1 className='text-primary'>Trabalhando com listas, e método Map()</h1>
+                <ul className='mt-3'>
+                    {lista.map((pokemon) => {
+                        return <p key={pokemon.id}>Nome: {pokemon.name} - Tipo: {pokemon.type}</p>
+                    })}
+                </ul>
+            </div>
         </div>
     )
 }
